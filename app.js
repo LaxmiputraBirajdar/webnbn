bc48c267238f641b5b5146653669473d799cf9bf
LaxmiputraBirajdar <laxmiputrabirajdar1515@gmail.com>
2023-02-28 20:57:50 +0530

    third commit

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require("express")
 const ejs = require("ejs")
-
-
-
 const app = express();
+
+
 
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
 
-    res.render("home")
+app.get("/", function(req, res)
+{
 
+  res.render("homa");
 })
+
+app.get('/:page', function(req, res) {
+  var page = req.params.page;
+  console.log(req.params.page);
+  
+  res.render(page);
+});
+
 
 
 app.listen(3000, function(){
